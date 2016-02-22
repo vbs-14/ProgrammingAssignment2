756395fccf26f4448279e4056aba9028d4a5504a
vbsoler <vbs-14@users.noreply.github.com>
2016-02-22 20:34:49 +0100

    final (small mistake corrected)

--- cachematrix.R
+++ cachematrix.R
@@ -17,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    inv <<- NULL
   }
   get <- function() x
   setinverse <- function(inverse) inv <<- inverse
